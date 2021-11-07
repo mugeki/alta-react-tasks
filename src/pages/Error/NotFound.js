cb55753a5cd67fc55f15aeb2c836625eb50d05d3
Muhamad Elang Ramadhan <elangmugeki@gmail.com>
2021-11-07 13:24:56 +0700

    push to github

--- src/pages/Error/NotFound.js
+++ src/pages/Error/NotFound.js
@@ -1,10 +1,19 @@
+import { useNavigate } from "react-router-dom";
+import styles from "./NotFound.module.css";
 import error from "./404NotFound.png";
 
 export default function NotFound() {
+	const navigate = useNavigate();
 	return (
 		<div className="position-absolute top-50 start-50 translate-middle">
-			<h1 className="fw-light">Page Not Found</h1>
 			<img className="w-75" src={error} alt="error" />
+			<h1 className="fw-light m-3">Page Not Found</h1>
+			<button
+				onClick={() => navigate("/")}
+				className={`nav-link mt-5 mx-auto ${styles.button}`}
+			>
+				Back to Home
+			</button>
 		</div>
 	);
 }
